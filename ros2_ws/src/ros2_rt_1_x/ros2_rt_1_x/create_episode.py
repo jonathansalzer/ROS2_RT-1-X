fixed scaling issues in finetuning code

ros2_ws/src/ros2_rt_1_x/ros2_rt_1_x/create_episode.py
@@ -14,6 +14,9 @@ class EpisodeLogger:
     def __init__(self, initial_pose: Pose, initial_grip: Float32):
         tf.config.experimental.set_visible_devices([], "GPU")
 
+        self.initpose = copy.deepcopy(initial_pose)
+        self.initgrip = copy.deepcopy(initial_grip)
+
         self.episode = []
         self.camera = camera.Camera()
         self.current_pose = copy.deepcopy(initial_pose)
@@ -25,17 +28,22 @@ class EpisodeLogger:
         self.natural_language_instruction = 'Pick up the yellow banana.'
 
     def log(self, new_pose, new_grip, terminate=False):
+        act = self._get_action(new_pose, new_grip, terminate)
+
+        self.current_pose = copy.deepcopy(new_pose)
+        self.current_grip = copy.deepcopy(new_grip)
+
         self.episode.append({
             'image': self._take_picture(),
-            'action': self._get_action(new_pose, new_grip, terminate),
+            'action': act,
             'language_instruction': self.natural_language_instruction,
             'state': self._get_state(terminate)
         })
+        
         self.episode_length += 1
         print(f'Logged action {self.episode_length}.')
         self.last_log_time = time.time()
-        self.current_pose = copy.deepcopy(new_pose)
-        self.current_grip = copy.deepcopy(new_grip)
+        
 
     def stop_and_save(self, filename):
         self.episode_started = False
@@ -46,6 +54,10 @@ class EpisodeLogger:
 
     def reset(self):
         self.episode = []
+        self.episode_length = 0
+        self.current_pose = self.initpose
+        self.current_grip = self.initgrip
+
 
     def _take_picture(self):
         image = Image.fromarray(cv2.cvtColor(self.camera.get_picture(), cv2.COLOR_BGRA2RGB)).convert('RGB')

ros2_ws/src/ros2_rt_1_x/ros2_rt_1_x/umi_dataset_explorer.py
@@ -16,32 +16,36 @@ def main():
     for file in os.listdir(directory):
         filename = os.fsdecode(file)
 
-        print(f"Correcting episode {filename}...")
+        print(f"Analyzing episode {filename}...")
 
         data = np.load(f"/home/jonathan/Thesis/ROS2_RT-1-X/ros2_ws/episodes/{filename}", allow_pickle=True)
 
-        corrected_data = []
+        highest_x = 0
+        lowest_x = 0
+        highest_y = 0
+        highest_z = 0
 
         for index, step in enumerate(data):
-            if index == len(data) - 1:
-                corrected_data.append({
-                    'image': step['image'],
-                    'action': step['action'],
-                    'language_instruction': step['language_instruction'],
-                    'state': calculate_final_state(data[index - 1]['state'], step['action'])
-                })
-            else:
-                corrected_data.append({
-                    'image': step['image'],
-                    'action': step['action'],
-                    'language_instruction': step['language_instruction'],
-                    'state': data[index + 1]['state']
-                })
+            world_vec = step['state']
+            x = world_vec[0]
+            y = world_vec[1]
+            z = world_vec[2]
 
-        # corrected_data[0]['state'] = STATE_0
-        # corrected_data[0]['action'] = ACTION_0
+            if x > highest_x:
+                highest_x = x
+            if x < lowest_x:
+                lowest_x = x
+            if y > highest_y:
+                highest_y = y
+            if z > highest_z:
+                highest_z = z
 
-        np.save(f"/home/jonathan/Thesis/ROS2_RT-1-X/ros2_ws/corrected_episodes/{filename}", corrected_data)
+        print(f"Highest x: {highest_x}")
+        print(f"Lowest x: {lowest_x}")
+        if highest_x > 0.4 or lowest_x < -0.4:
+            print(f" ================> Episode {filename} has x values out of range.")
+        # print(f"Highest y: {highest_y}")
+        # print(f"Highest z: {highest_z}")
 
 def calculate_final_state(prev_state, action):
     return [